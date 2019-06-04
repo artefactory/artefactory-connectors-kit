16a6c07032d86829caa34b25d9214325c922f63b
Douglas Willcocks <douglas@d-tw.org>
2019-06-04 14:34:09 +0200

    Better function name propagation

--- lib/utils/retry.py
+++ lib/utils/retry.py
@@ -1,15 +1,11 @@
 from tenacity import retry as _retry, wait_exponential, before_sleep_log, before_log
-from functools import update_wrapper
 import config
 import logging
 
 
 def retry(fn):
-        @_retry(wait=wait_exponential(multiplier=1, min=4, max=10),
-                reraise=True,
-                before=before_log(config.logger, logging.INFO),
-                before_sleep=before_sleep_log(config.logger, logging.INFO))
-        def retry_wrapper(*args, **kwargs):
-            return fn(*args, **kwargs)
 
-        return update_wrapper(retry_wrapper, fn)
+    return _retry(wait=wait_exponential(multiplier=1, min=4, max=10),
+                  reraise=True,
+                  before=before_log(config.logger, logging.DEBUG),
+                  before_sleep=before_sleep_log(config.logger, logging.DEBUG))(fn)
