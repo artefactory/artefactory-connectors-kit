27692b79c64ff3948fb8df5733e42f87542edc81
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2019-07-11 13:48:34 +0200

    finished minimal working version facebook_connector

--- lib/helpers/facebook_marketing_helper.py
+++ lib/helpers/facebook_marketing_helper.py
@@ -1,17 +1,21 @@
 from facebook_business.adobjects.adsinsights import AdsInsights
 
-BREAKDOWNS_POSSIBLE_VALUES = [v for k,v in AdsInsights.Breakdowns.__dict__.items() if not k.startswith("__")]
-
-ACTION_BREAKDOWNS_POSSIBLE_VALUES = [v for k,v in AdsInsights.ActionBreakdowns.__dict__.items() if not k.startswith("__")]
-
-AD_OBJECT_TYPES = ['ad_account_object',
-'ad_campaign_object',
-'adset_object',
-'ad_object']
-
-LEVEL = [
-'ad', 
-'adset', 
-'campaign', 
-'account'
+BREAKDOWNS_POSSIBLE_VALUES = [
+    v for k, v in AdsInsights.Breakdowns.__dict__.items() if not k.startswith("__")
 ]
+
+ACTION_BREAKDOWNS_POSSIBLE_VALUES = [
+    v
+    for k, v in AdsInsights.ActionBreakdowns.__dict__.items()
+    if not k.startswith("__")
+]
+
+AD_OBJECT_TYPES = [
+    "ad_account_object",
+    "ad_campaign_object",
+    "adset_object",
+    "ad_object",
+    "ad_user",
+]
+
+LEVELS_POSSIBLE_VALUES = ["ad", "adset", "campaign", "account"]
--- lib/readers/__init__.py
+++ lib/readers/__init__.py
@@ -5,6 +5,7 @@ from lib.readers.gcs_reader import gcs
 from lib.readers.oracle_reader import oracle
 from lib.readers.gsheets_reader import gsheets
 from lib.readers.salesforce_reader import salesforce
+from lib.readers.facebook_reader import facebook_marketing
 
 
 readers = [
@@ -12,6 +13,7 @@ readers = [
     salesforce,
     gsheets,
     gcs,
+    facebook_marketing,
     oracle
 ]
 
