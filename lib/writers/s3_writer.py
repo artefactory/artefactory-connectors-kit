e83d60a06f8e25b9a4322cbefff5e2e353cc8da0
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2019-07-22 12:03:26 +0200

    finished and tested s3 writer

--- lib/writers/s3_writer.py
+++ lib/writers/s3_writer.py
@@ -15,8 +15,8 @@ from lib.utils.retry import retry
 
 @click.command(name="write_s3")
 @click.option('--s3-bucket-name', help="S3 Bucket name", required=True)
-@click.option('--s3-bucket-region', required=True, default = 'us-east-2')
-@click.option('--s3-acess-key-id', required=True)
+@click.option('--s3-bucket-region', required=True)
+@click.option('--s3-access-key-id', required=True)
 @click.option('--s3-access-key-secret', required=True)
 @processor()
 def s3(**kwargs):
@@ -26,28 +26,32 @@ def s3(**kwargs):
 class S3Writer(Writer):
 
 
-    def __init__(self, bucket_name, acess_key_id, access_key_secret, bucket_region, **kwargs):
+    def __init__(self, bucket_name, access_key_id, access_key_secret, bucket_region, **kwargs):
         boto_config = {
                        'region_name': bucket_region,
-                       'aws_access_key_id': acess_key_id,
+                       'aws_access_key_id': access_key_id,
                        'aws_secret_access_key': access_key_secret
                        }
-        self.bucket_name = bucket_name
-        self.bucket_region = bucket_region
-        self.s3_resource = boto3.resource('s3', **boto_config)
-        self.s3_client = boto3.client('s3', **boto_config)
-
+        self._bucket_name = bucket_name
+        self._bucket_region = bucket_region
+        self._s3_resource = boto3.resource('s3', **boto_config)
+    
     @retry
     def write(self, stream):
 
         logging.info("Writing file to S3")
-        bucket = self.s3_resource.Bucket(self.bucket_name) 
+        bucket = self._s3_resource.Bucket(self._bucket_name) 
 
-        if bucket in self.s3_resource.buckets.all():
-            self.s3_resource.create_bucket(Bucket=self.bucket_name, CreateBucketConfiguration={'LocationConstraint': self.bucket_region})
+        if not bucket in self._s3_resource.buckets.all():
+            self._s3_resource.create_bucket(Bucket=self._bucket_name, CreateBucketConfiguration={'LocationConstraint': self._bucket_region})
+        
+        bucket_region = self._s3_resource.meta.client.get_bucket_location(Bucket = self._bucket_name)['LocationConstraint']
+       
+        #if the bucket region doesn't match the presigned url generated, will not work
+        assert bucket_region == self._bucket_region, "the region you provided ({}) does'nt match the bucket's found region : ({}) ".format(self._bucket_region, bucket_region)
         
         bucket.upload_fileobj(stream.as_file(), stream.name)
-        url_file =  self.s3_client.generate_presigned_url('get_object', Params={'Bucket': self.bucket_name,'Key': stream.name}, ExpiresIn=3600)
+        url_file =  self._s3_resource.meta.client.generate_presigned_url('get_object', Params={'Bucket': self._bucket_name,'Key': stream.name}, ExpiresIn=3600)
 
         return url_file, bucket
 
