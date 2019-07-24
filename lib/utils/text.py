77fdf268acb8c3b8e305bc6ac1b8919c1be34b20
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2019-07-24 11:21:59 +0200

    dbm implementation finished, waiting for tests

---
+++ lib/utils/text.py
@@ -0,0 +1,23 @@
+
+def add_column_value_to_csv_line_iterator(line_iterator, columname, value):
+    first_line = True
+    for line in line_iterator:
+        if first_line:
+            first_line = False
+            if columname in line.split(','):
+                raise Exception('Column {} already present'.format(columname))
+            yield line + ',' + columname
+        else:
+            yield line + ',' + value
+
+
+def get_generator_dict_from_str_csv(line_iterator):
+    got_header = False
+    headers = []
+    for line in line_iterator:
+        if not got_header:
+            headers = line.decode("utf-8").split(',')
+            got_header = True
+        else :
+            values = line.decode("utf-8").split(',')
+            yield {headers[i]:values[i] for i in range(len(headers))}
