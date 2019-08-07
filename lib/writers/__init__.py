26acc3a7523ba7b1ef95ec37069a6753eaabb371
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2019-08-07 11:53:54 +0200

    remove 's3://' from prefix

--- lib/writers/__init__.py
+++ lib/writers/__init__.py
@@ -4,13 +4,15 @@ from lib.writers.gcs_writer import gcs
 from lib.writers.console_writer import console
 from lib.writers.local_writer import local
 from lib.writers.bigquery_writer import bq
+from lib.writers.s3_writer import s3
 
 
 writers = [
     gcs,
     console,
     local,
-    bq
+    bq,
+    s3
     # "oracle": oracle,
     # "gsheets": gsheets,
     # "salesforce": salesforce
