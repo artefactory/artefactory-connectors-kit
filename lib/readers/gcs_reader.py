2e6667447376018fc0d20712ef321a1132ec83a1
Nathan <nathan.vermeersch@artefact.com>
2019-09-10 11:56:53 +0200

    refactored GCSReader with ObjectStorageReader as parent class

--- lib/readers/gcs_reader.py
+++ lib/readers/gcs_reader.py
@@ -3,17 +3,13 @@ import config
 import os
 import tempfile
 import csv
+import codecs
 import logging
 
 from google.cloud import storage
 from lib.commands.command import processor
-from lib.readers.reader import Reader
+from lib.readers.objectstorage_reader import ObjectStorageReader
 from lib.utils.args import extract_args
-from lib.streams.normalized_json_stream import NormalizedJSONStream
-
-
-GCS_MAX_TIMESTAMP_STATE_KEY = "gcs_max_timestamp"
-GCS_MAX_FILES_STATE_KEY = "gcs_max_files"
 
 
 @click.command(name="read_gcs")
@@ -27,91 +23,32 @@ def gcs(**kwargs):
     return GCSReader(**extract_args('gcs_', kwargs))
 
 
-class GCSReader(Reader):
+class GCSReader(ObjectStorageReader):
 
-    def __init__(self, bucket, prefix, format, key_filter, **kwargs):
-        self._client = storage.Client(project=config.PROJECT_ID)
-        self._bucket = self._client.bucket(bucket)
-        self._prefix = prefix
-        self._key_filter = key_filter
+    def __init__(self, bucket, prefix, format, **kwargs):
+        super(GCSReader, self).__init__(bucket, prefix, format, platform="GCS", **kwargs)
 
-        if format == 'csv':
-            self._reader = self._make_csv_reader(**kwargs)
+    def create_client(self, config):
+        return storage.Client(project=config.PROJECT_ID)
 
-    def read(self):
+    def create_bucket(self, client, bucket):
+        return client.bucket(bucket)
 
-        for prefix in self._prefix:
-
-            blobs_sorted_by_time = sorted(self._bucket.list_blobs(prefix=prefix), key=lambda b: b.updated)
-
-            for blob in blobs_sorted_by_time:
-
-                logging.info("Found GCS file %s", blob.path)
-
-                if self.has_already_processed_blob(blob):
-                    logging.info("Skipping already processed file %s", blob.path)
-                    continue
-
-                def result_generator():
-                    temp = tempfile.TemporaryFile()
-                    blob.download_to_file(temp)
-
-                    for record in self._reader(temp):
-                        yield record
-
-                    self.checkpoint_blob(blob)
-
-                name = os.path.basename(blob.path)
-
-                yield NormalizedJSONStream(name, result_generator())
-
-    def has_already_processed_blob(self, blob):
-
-        assert blob.updated is not None
-
-        max_timestamp = self.state.get(GCS_MAX_TIMESTAMP_STATE_KEY)
-
-        # We haven't seen any files before
-        if not max_timestamp:
-            return False
-
-        # The most recent file is more recent than this one
-        if max_timestamp > blob.updated:
-            return True
-
-        # The most recent file is less recent than this one
-        if max_timestamp < blob.updated:
-            return False
-
-        # If the timestamp is the same, then check if we kept it
-        if max_timestamp == blob.updated:
-            max_files = self.state.get(GCS_MAX_FILES_STATE_KEY)
-            return blob.path in max_files
-
-    def checkpoint_blob(self, blob):
-
-        assert blob.updated is not None
-
-        max_timestamp = self.state.get(GCS_MAX_TIMESTAMP_STATE_KEY)
-
-        assert blob.updated >= max_timestamp
-
-        if not max_timestamp or max_timestamp < blob.updated:
-            self.state.set(GCS_MAX_TIMESTAMP_STATE_KEY, blob.updated)
-            self.state.set(GCS_MAX_FILES_STATE_KEY, [blob.path])
-            return
-
-        if max_timestamp == blob.updated:
-            max_files = self.state.get(GCS_MAX_FILES_STATE_KEY)
-            max_files.append(blob.updated)
-            self.state.set(GCS_MAX_FILES_STATE_KEY, max_files)
-            return
+    def list_objects(self, bucket, prefix):
+        return bucket.list_blobs(prefix=prefix)
 
     @staticmethod
-    def _make_csv_reader(csv_delimiter):
+    def get_timestamp(o):
+        return o.updated
 
-        def read_csv(fd):
-            fd.seek(0)
-            return csv.DictReader(fd, delimiter=str(csv_delimiter))
+    @staticmethod
+    def get_key(o):
+        return o.path
 
-        return read_csv
+    @staticmethod
+    def to_object(o):
+        return o
+
+    @staticmethod
+    def download_object_to_file(o, temp):
+        o.download_to_file(temp)
---
+++ lib/readers/objectstorage_reader.py
@@ -0,0 +1,142 @@
+import config
+import os
+import tempfile
+import logging
+import csv, codecs
+
+from lib.readers.reader import Reader
+from lib.streams.normalized_json_stream import NormalizedJSONStream
+
+
+class ObjectStorageReader(Reader):
+
+    def __init__(self, bucket, prefix, file_format, platform=None, **kwargs):
+        self._client = self.create_client(config)
+        self._bucket = self.create_bucket(self._client, bucket)
+        self._prefix = prefix
+        self._platform = platform
+
+        self._format = file_format
+
+        if self._format == 'csv':
+            self._reader = self._make_csv_reader(**kwargs)
+
+        self.MAX_TIMESTAMP_STATE_KEY = "{}_max_timestamp".format(self._platform).lower()
+        self.MAX_FILES_STATE_KEY = "{}_max_files".format(self._platform).lower()
+
+    def read(self):
+
+        for prefix in self._prefix:
+
+            objects_sorted_by_time = sorted(self.list_objects(bucket=self._bucket, prefix=prefix),
+                                            key=lambda o: self.get_timestamp(o))
+
+            for o in objects_sorted_by_time:
+
+                o = self.to_object(o)
+
+                logging.info("Found %s file %s" % (self._platform, self.get_key(o)))
+
+                if not self.compatible_object(o):
+                    logging.info("Wrong extension: Skipping file %s", self.get_key(o))
+                    continue
+
+                if self.has_already_processed_object(o):
+                    logging.info("Skipping already processed file %s", self.get_key(o))
+                    continue
+
+                def result_generator():
+                    temp = tempfile.TemporaryFile()
+                    self.download_object_to_file(o, temp)
+
+                    for record in self._reader(temp):
+                        yield record
+
+                    self.checkpoint_object(o)
+
+                name = os.path.basename(self.get_key(o))
+
+                yield NormalizedJSONStream(name, result_generator())
+
+    def compatible_object(self, o):
+        return self.get_key(o).endswith('.' + self._format)
+
+    def has_already_processed_object(self, o):
+
+        assert self.get_timestamp(o) is not None
+
+        max_timestamp = self.state.get(self.MAX_TIMESTAMP_STATE_KEY)
+
+        # We haven't seen any files before
+        if not max_timestamp:
+            return False
+
+        logging.warning(str(max_timestamp))
+        logging.warning(str(self.get_timestamp(o)))
+
+        # The most recent file is more recent than this one
+        if max_timestamp > self.get_timestamp(o):
+            return True
+
+        # The most recent file is less recent than this one
+        if max_timestamp < self.get_timestamp(o):
+            return False
+
+        # If the timestamp is the same, then check if we kept it
+        if max_timestamp == self.get_timestamp(o):
+            max_files = self.state.get(self.MAX_FILES_STATE_KEY)
+            return self.get_key(o) in max_files
+
+    def checkpoint_object(self, o):
+
+        assert self.get_timestamp(o) is not None
+
+        max_timestamp = self.state.get(self.MAX_TIMESTAMP_STATE_KEY)
+
+        if max_timestamp:
+            assert self.get_timestamp(o) >= max_timestamp
+
+        if not max_timestamp or max_timestamp < self.get_timestamp(o):
+            self.state.set(self.MAX_TIMESTAMP_STATE_KEY, self.get_timestamp(o))
+            self.state.set(self.MAX_FILES_STATE_KEY, [self.get_key(o)])
+            return
+
+        if max_timestamp == self.get_timestamp(o):
+            max_files = self.state.get(self.MAX_FILES_STATE_KEY)
+            max_files.append(self.get_timestamp(o))
+            self.state.set(self.MAX_FILES_STATE_KEY, max_files)
+            return
+
+    def create_client(self, config):
+        raise NotImplementedError
+
+    def create_bucket(self, client, bucket):
+        raise NotImplementedError
+
+    def list_objects(self, bucket, prefix):
+        raise NotImplementedError
+
+    @staticmethod
+    def get_timestamp(o):
+        raise NotImplementedError
+
+    @staticmethod
+    def get_key(o):
+        raise NotImplementedError
+
+    @staticmethod
+    def to_object(o):
+        raise NotImplementedError
+
+    @staticmethod
+    def download_object_to_file(o, temp):
+        raise NotImplementedError
+
+    @staticmethod
+    def _make_csv_reader(csv_delimiter, **kwargs):
+
+        def read_csv(fd):
+            fd.seek(0)
+            return csv.DictReader(codecs.iterdecode(fd, encoding='utf-8'), delimiter=str(csv_delimiter))
+
+        return read_csv
