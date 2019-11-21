2fd5feac59ffcd82a312c94a5eb5d2213a6d6b84
benoitgoujon <benoit.goujon@artefact.com>
2019-11-21 16:23:24 +0100

    Fixed linter requirements.

--- tests/utils/test_text_utils.py
+++ tests/utils/test_text_utils.py
@@ -11,7 +11,13 @@ def test_multiple_encodings():
         '1130016,0,,"    ",0.00,4080863'
     )
     lines = [
-        b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,Advertiser Status,Advertiser Integration Code,Insertion Order,Insertion Order ID,Insertion Order Status,Insertion Order Integration Code,Line Item,Line Item ID,Line Item Status,Line Item Integration Code,Targeted Data Providers,Cookie Reach: Average Impression Frequency,Cookie Reach: Impression Reach",
+        (b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,"
+         b"Advertiser Status,Advertiser Integration Code,Insertion Order,"
+         b"Insertion Order ID,Insertion Order Status,Insertion Order "
+         b"Integration Code,Line Item,Line Item ID,Line Item Status,"
+         b"Line Item Integration Code,Targeted Data Providers,"
+         b"Cookie Reach: Average Impression Frequency,Cookie Reach: "
+         b"Impression Reach"),
         test_string_to_encode.encode("utf-8"),
         test_string_to_encode.encode("ascii"),
         test_string_to_encode.encode("windows-1252"),
@@ -27,11 +33,17 @@ def test_multiple_encodings():
         "Advertiser ID": "3992233",
         "Advertiser Status": "0",
         "Advertiser Integration Code": "",
-        "Insertion Order": "YR_Sanofi_Allegra_201910_Consideration_DV360_Precision_Native-Ads_Cross-Device_BR",
+        "Insertion Order": (
+            "YR_Sanofi_Allegra_201910_Consideration_DV360"
+            "_Precision_Native-Ads_Cross-Device_BR"
+        ),
         "Insertion Order ID": "11140383",
         "Insertion Order Status": "Active",
         "Insertion Order Integration Code": "",
-        "Line Item": "YR_Sanofi_Allegra_201910_Consideration_DV360_Precision_OA_Native-Ads_DV-Affinity-Health_Desktop_BR",
+        "Line Item": (
+            "YR_Sanofi_Allegra_201910_Consideration_DV360_Precision_OA_Native"
+            "-Ads_DV-Affinity-Health_Desktop_BR"
+        ),
         "Line Item ID": "1130016",
         "Line Item Status": "0",
         "Line Item Integration Code": "",
@@ -47,7 +59,13 @@ def test_multiple_encodings():
 
 def test_blank_line():
     lines = [
-        b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,Advertiser Status,Advertiser Integration Code,Insertion Order,Insertion Order ID,Insertion Order Status,Insertion Order Integration Code,Line Item,Line Item ID,Line Item Status,Line Item Integration Code,Targeted Data Providers,Cookie Reach: Average Impression Frequency,Cookie Reach: Impression Reach",
+        (b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,"
+         b"Advertiser Status,Advertiser Integration Code,Insertion Order,"
+         b"Insertion Order ID,Insertion Order Status,Insertion Order "
+         b"Integration Code,Line Item,Line Item ID,Line Item Status,"
+         b"Line Item Integration Code,Targeted Data Providers,"
+         b"Cookie Reach: Average Impression Frequency,Cookie Reach: "
+         b"Impression Reach"),
         ""
     ]
     line_iterator_with_blank_line = (line for line in lines)
@@ -55,7 +73,13 @@ def test_blank_line():
         line_iterator_with_blank_line
     )
 
-    lines.insert(1, b'BR,Sanofi Aventis Brasil,3945535,Active,Allegra,3992233,0,,YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360_Cross-Device_BR,8674464,Active,,YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR,26143278,0,,"",0.00,41')
+    lines.insert(
+        1,
+        (b'BR,Sanofi Aventis Brasil,3945535,Active,Allegra,3992233,'
+         b'0,,YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360'
+         b'_Cross-Device_BR,8674464,Active,,YR_Sanofi_Allegra_Hardsell'
+         b'_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR'
+         b',26143278,0,,"",0.00,41'))
     expected_dict = {
         "Country": "BR",
         "Partner": "Sanofi Aventis Brasil",
@@ -65,11 +89,17 @@ def test_blank_line():
         "Advertiser ID": "3992233",
         "Advertiser Status": "0",
         "Advertiser Integration Code": "",
-        "Insertion Order": "YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360_Cross-Device_BR",
+        "Insertion Order": (
+            "YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360"
+            "_Cross-Device_BR"
+        ),
         "Insertion Order ID": "8674464",
         "Insertion Order Status": "Active",
         "Insertion Order Integration Code": "",
-        "Line Item": "YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR",
+        "Line Item": (
+            "YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health"
+            "-Beauty-Buyers-Allergy_Desktop_BR"
+        ),
         "Line Item ID": "26143278",
         "Line Item Status": "0",
         "Line Item Integration Code": "",
@@ -95,8 +125,18 @@ def test_blank_line():
 
 def test_invalid_byte():
     lines = [
-        b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,Advertiser Status,Advertiser Integration Code,Insertion Order,Insertion Order ID,Insertion Order Status,Insertion Order Integration Code,Line Item,Line Item ID,Line Item Status,Line Item Integration Code,Targeted Data Providers,Cookie Reach: Average Impression Frequency,Cookie Reach: Impression Reach",
-        b'BR,Sanofi Aventis Brasil,3945535,Active,Allegra,3992233,0,,YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360_Cross-Device_BR,8674464,Active,,YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR,26143278,0,,"   \x91\xea\xd0$",0.00,41',
+        (b"Country,Partner,Partner ID,Partner Status,Advertiser,Advertiser ID,"
+         b"Advertiser Status,Advertiser Integration Code,Insertion Order,"
+         b"Insertion Order ID,Insertion Order Status,Insertion Order "
+         b"Integration Code,Line Item,Line Item ID,Line Item Status,"
+         b"Line Item Integration Code,Targeted Data Providers,"
+         b"Cookie Reach: Average Impression Frequency,Cookie Reach: "
+         b"Impression Reach"),
+        (b'BR,Sanofi Aventis Brasil,3945535,Active,Allegra,3992233,'
+         b'0,,YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360'
+         b'_Cross-Device_BR,8674464,Active,,YR_Sanofi_Allegra_Hardsell'
+         b'_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR'
+         b',26143278,0,,"   \x91\xea\xd0$",0.00,41'),
     ]
     line_iterator_invalid_byte = (line for line in lines)
     expected_dict = {
@@ -108,11 +148,17 @@ def test_invalid_byte():
         "Advertiser ID": "3992233",
         "Advertiser Status": "0",
         "Advertiser Integration Code": "",
-        "Insertion Order": "YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360_Cross-Device_BR",
+        "Insertion Order": (
+            "YR_Sanofi_Awareness_2019_Allegra_Hardsell_Display_DV360"
+            "_Cross-Device_BR"
+        ),
         "Insertion Order ID": "8674464",
         "Insertion Order Status": "Active",
         "Insertion Order Integration Code": "",
-        "Line Item": "YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health-Beauty-Buyers-Allergy_Desktop_BR",
+        "Line Item": (
+            "YR_Sanofi_Allegra_Hardsell_Display_Datalogix-Health-Beauty"
+            "-Buyers-Allergy_Desktop_BR"
+        ),
         "Line Item ID": "26143278",
         "Line Item Status": "0",
         "Line Item Integration Code": "",
