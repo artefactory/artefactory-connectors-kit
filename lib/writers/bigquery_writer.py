47cc462fe8da8af2c508f3bb899a58939e08f562
Vivien MORLET <vivienmorlet@FRART0119M.local>
2019-11-29 14:45:05 +0100

    mini refacto bq_writer

--- lib/writers/bigquery_writer.py
+++ lib/writers/bigquery_writer.py
@@ -15,27 +15,25 @@ from lib.helpers.google_base import GoogleBaseClass
 
 
 @click.command(name="write_bq")
-@click.option('--bq-dataset', required=True)
-@click.option('--bq-table', required=True)
-@click.option('--bq-bucket', required=True)
-@click.option('--bq-partition-column')
-@click.option('--bq-write-disposition', default="truncate",
-              type=click.Choice(['truncate', 'append']))
-@click.option('--bq-location', default="EU", type=click.Choice(['EU', 'US']))
-@click.option('--bq-keep-files', is_flag=True, default=False)
+@click.option("--bq-dataset", required=True)
+@click.option("--bq-table", required=True)
+@click.option("--bq-bucket", required=True)
+@click.option("--bq-partition-column")
+@click.option("--bq-write-disposition", default="truncate", type=click.Choice(["truncate", "append"]))
+@click.option("--bq-location", default="EU", type=click.Choice(["EU", "US"]))
+@click.option("--bq-keep-files", is_flag=True, default=False)
 @processor()
 def bq(**kwargs):
-    return BigQueryWriter(**extract_args('bq_', kwargs))
+    return BigQueryWriter(**extract_args("bq_", kwargs))
 
 
 class BigQueryWriter(Writer, GoogleBaseClass):
     _client = None
 
-    def __init__(self, dataset, table, bucket, partition_column, write_disposition,
-                 location, keep_files):
+    def __init__(self, dataset, table, bucket, partition_column, write_disposition, location, keep_files):
 
-        self._client = bigquery.Client(credentials=self._get_credentials(),
-                                       project=config.PROJECT_ID)
+        self._project_id = config.PROJECT_ID
+        self._client = bigquery.Client(credentials=self._get_credentials(), project=self._project_id)
         self._dataset = dataset
         self._table = table
         self._bucket = bucket
@@ -49,18 +47,17 @@ class BigQueryWriter(Writer, GoogleBaseClass):
 
         normalized_stream = NormalizedJSONStream.create_from_stream(stream)
 
-        gcs_writer = GCSWriter(self._bucket)
+        gcs_writer = GCSWriter(self._bucket, self._project_id)
         gcs_uri, blob = gcs_writer.write(normalized_stream)
 
         table_ref = self._get_table_ref()
 
-        load_job = self._client.load_table_from_uri(gcs_uri, table_ref,
-                                                    job_config=self.job_config())
+        load_job = self._client.load_table_from_uri(gcs_uri, table_ref, job_config=self.job_config())
 
         logging.info("Loading data into BigQuery %s:%s", self._dataset, self._table)
         result = load_job.result()
 
-        assert result.state == 'DONE'
+        assert result.state == "DONE"
 
         if not self._keep_files:
             logging.info("Deleting GCS file: %s", gcs_uri)
@@ -80,17 +77,16 @@ class BigQueryWriter(Writer, GoogleBaseClass):
         job_config.source_format = bigquery.job.SourceFormat.NEWLINE_DELIMITED_JSON
         job_config.autodetect = True
 
-        if self._write_disposition == 'truncate':
+        if self._write_disposition == "truncate":
             job_config.write_disposition = bigquery.job.WriteDisposition.WRITE_TRUNCATE
-        elif self._write_disposition == 'append':
+        elif self._write_disposition == "append":
             job_config.write_disposition = bigquery.job.WriteDisposition.WRITE_APPEND
         else:
             raise Exception("Unknown BigQuery write disposition")
 
         if self._partition_column:
             job_config.time_partitioning = bigquery.table.TimePartitioning(
-                bigquery.table.TimePartitioningType.DAY,
-                self._partition_column
+                bigquery.table.TimePartitioningType.DAY, self._partition_column
             )
 
         return job_config
