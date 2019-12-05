3a0b9370ca92a21f05e2d23f16e1a5c9bc7b07d1
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2019-12-05 23:55:55 +0100

    adobe_helper methods added

--- lib/helpers/adobe_helper.py
+++ lib/helpers/adobe_helper.py
@@ -1,5 +1,28 @@
+import datetime
+import binascii
+import uuid
+import hashlib
+import json
 
 
-def get_report_endpoin(company_id):
-    return f"https://analytics.adobe.io/api/{company_id}/reports"
+def _serialize_header(properties):
+    header = ['{key}="{value}"'.format(key=k, value=v) for k, v in properties.items()]
+    return ', '.join(header)
 
+
+def build_headers(password, username):
+        nonce = str(uuid.uuid4())
+        base64nonce = binascii.b2a_base64(binascii.a2b_qp(nonce))
+        created_date = datetime.datetime.utcnow().isoformat() + 'Z'
+        sha = nonce + created_date + password
+        sha_object = hashlib.sha1(sha.encode())
+        password_64 = binascii.b2a_base64(sha_object.digest())
+        
+        properties = {
+            "Username": username,
+            "PasswordDigest": password_64.decode().strip(),
+            "Nonce": base64nonce.decode().strip(),
+            "Created": created_date
+        }
+        header = 'UsernameToken ' + _serialize_header(properties)
+        return {'X-WSSE': header}
--- lib/readers/adobe_reader.py
+++ lib/readers/adobe_reader.py
@@ -3,8 +3,11 @@ import config
 import os
 import logging
 import httplib2
-
 import datetime
+import binascii
+import uuid
+import hashlib
+import json
 
 from itertools import chain
 from lib.commands.command import processor
@@ -12,6 +15,8 @@ from lib.readers.reader import Reader
 from lib.utils.args import extract_args
 from lib.utils.retry import retry
 from lib.streams.json_stream import JSONStream
+import requests
+from lib.helpers.adobe_helper import build_headers
 
 
 DISCOVERY_URI = "https://analyticsreporting.googleapis.com/$discovery/rest"
@@ -23,9 +28,10 @@ ADOBE_API_ENDPOINT = "https://api.omniture.com/admin/1.4/rest/"
 @click.command(name="read_adobe")
 @click.option("--adobe-password", required=True)
 @click.option("--adobe-username", required=True)
+@click.option("--adobe-list-report-suite")
 @click.option("--adobe-report-suite-id")
-@click.option("--adobe-report-elements", type=click.Tuple([str, int]), multiple = True)
-@click.option("--adobe-report-dimensions", type=click.Tuple([str, int]), multiple = True)
+@click.option("--adobe-report-element-id", multiple = True)
+@click.option("--adobe-report-dimension-id", multiple = True)
 @click.option(
     "--adobe-day-range",
     type=click.Choice(["PREVIOUS_DAY", "LAST_30_DAYS", "LAST_7_DAYS", "LAST_90_DAYS"]),
@@ -39,5 +45,23 @@ def adobe(**kwargs):
 
 
 class AdobeReader(Reader):
-    def __init__(self,**kwargs):
-      pass
+    def __init__(self,password, username, **kwargs):
+      self.password = password
+      self.username = username
+    
+    @staticmethod
+    def request(api, method, data=None):
+        """ Compare with https://marketing.adobe.com/developer/api-explorer """
+        api_method = '{0}.{1}'.format(api, method)
+        data = data or dict()
+        # logger.info("{}.{} {}".format(api, method, data))
+        response = requests.post(
+            ADOBE_API_ENDPOINT,
+            params={'method': api_method},
+            data=json.dumps(data),
+            headers= build_headers(self.password, self.username)
+        )
+        json_response = response.json()
+        #logger.debug("Response: {}".format(json_response))
+        return json_response
+    
