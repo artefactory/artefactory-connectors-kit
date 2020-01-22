99237cf3dc99f5e3a7df5e507e31bf6e4e648310
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2020-01-22 15:45:30 +0100

    fix get report latency

--- lib/readers/adobe_reader.py
+++ lib/readers/adobe_reader.py
@@ -132,11 +132,12 @@ class AdobeReader(Reader):
 
     @retry
     def get_report(self, report_id, page_number=1):
-        response = self.request(
+        request_f = lambda : self.request(
             api="Report",
             method="Get",
             data={"reportID": report_id, "page": page_number},
         )
+        response = request_f()
         idx = 1
         while response.get("error") == "report_not_ready":
             logging.info(f"waiting {idx} s for report to be ready")
@@ -144,6 +145,7 @@ class AdobeReader(Reader):
             if idx + 1 > MAX_WAIT_REPORT_DELAY:
                 raise ReportNotReadyError(f"waited too long for report to be ready")
             idx = idx * 2
+            response = request_f()
         return response
 
     def download_report(self, rep_id):
