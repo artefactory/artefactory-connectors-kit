d986c76dc180ff8316a9d2bf1546aa692b50fc95
rogermichel <rmlaunay@gmail.com>
2020-02-04 17:42:00 +0800

    debug segment_id and segment-id

--- nck/readers/ga_reader.py
+++ nck/readers/ga_reader.py
@@ -27,7 +27,7 @@ DATEFORMAT = "%Y-%m-%d"
 @click.option("--ga-account-id", default=[], multiple=True)
 @click.option("--ga-dimension", multiple=True)
 @click.option("--ga-metric", multiple=True)
-@click.option("--ga-segment-id", multiple=True)
+@click.option("--ga-segment_id", multiple=True)
 @click.option("--ga-start-date", type=click.DateTime(), default=None)
 @click.option("--ga-end-date", type=click.DateTime(), default=None)
 @click.option("--ga-date-range", nargs=2, type=click.DateTime(), default=None)
@@ -130,7 +130,7 @@ class GaReader(Reader):
     def get_view_id_report_request(self, view_id):
         metrics = self.kwargs.get("metric", [])
         dimensions = self.kwargs.get("dimension", [])
-        segment_ids = self.kwargs.get("segment-id", [])
+        segment_ids = self.kwargs.get("segment_id", [])
         report_request = {
             "viewId": view_id,
             "metrics": [{"expression": metric} for metric in metrics],
