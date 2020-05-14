fbe1cd0632b864fc5658ec9b192dc35ac8266c98
gabrielleberanger <gabrielle.beranger@gmail.com>
2020-05-14 14:55:05 +0200

    Fix tests code formatting

--- tests/readers/test_facebook_reader.py
+++ tests/readers/test_facebook_reader.py
@@ -26,6 +26,7 @@ from facebook_business.api import FacebookAdsApi
 from facebook_business.adobjects.adsinsights import AdsInsights
 from facebook_business.adobjects.ad import Ad
 
+
 class FacebookReaderTest(TestCase):
 
     DATEFORMAT = "%Y-%m-%d"
@@ -45,20 +46,53 @@ class FacebookReaderTest(TestCase):
         "start_date": None,
         "end_date": None,
         "date_preset": None,
-        "add_date_to_report": False
+        "add_date_to_report": False,
     }
 
     @parameterized.expand(
         [
-            ("incompatible_level",{"object_type": "ad", "level": "account"}),
-            ("missing_breakdown", {"ad_insights": True, "field": ["age"], "breakdown": []}),
-            ("missing_action_breakdown",{"ad_insights": True, "field": ["actions[action_type:link_click]"], "action_breakdown": []}),
-            ("creative_level_for_adinsights_query", {"ad_insights": True, "object_type": "creative", "level": "creative"}),
-            ("breakdown_for_object_node_query", {"ad_insights": False, "breakdown": ["age"]}),
-            ("action_breakdown_for_object_node_query", {"ad_insights": False, "action_breakdown": ["action_type"]}),
-            ("time_range_for_object_node_query", {"ad_insights": False, "level": "account", "start_date": "2020-01-01", "end_date": "2020-01-01"}),
-            ("date_preset_for_object_node_query", {"ad_insights": False, "level": "account", "date_preset": "last_30d"}),
-            ("time_increment_for_object_node_query", {"ad_insights": False, "time_increment": "1"})
+            ("incompatible_level", {"object_type": "ad", "level": "account"}),
+            (
+                "missing_breakdown",
+                {"ad_insights": True, "field": ["age"], "breakdown": []},
+            ),
+            (
+                "missing_action_breakdown",
+                {
+                    "ad_insights": True,
+                    "field": ["actions[action_type:link_click]"],
+                    "action_breakdown": [],
+                },
+            ),
+            (
+                "creative_level_for_adinsights_query",
+                {"ad_insights": True, "object_type": "creative", "level": "creative"},
+            ),
+            (
+                "breakdown_for_object_node_query",
+                {"ad_insights": False, "breakdown": ["age"]},
+            ),
+            (
+                "action_breakdown_for_object_node_query",
+                {"ad_insights": False, "action_breakdown": ["action_type"]},
+            ),
+            (
+                "time_range_for_object_node_query",
+                {
+                    "ad_insights": False,
+                    "level": "account",
+                    "start_date": "2020-01-01",
+                    "end_date": "2020-01-01",
+                },
+            ),
+            (
+                "date_preset_for_object_node_query",
+                {"ad_insights": False, "level": "account", "date_preset": "last_30d"},
+            ),
+            (
+                "time_increment_for_object_node_query",
+                {"ad_insights": False, "time_increment": "1"},
+            ),
         ]
     )
     def test_refuse_invalid_input(self, name, parameters):
@@ -71,25 +105,37 @@ class FacebookReaderTest(TestCase):
         temp_kwargs = self.kwargs.copy()
         temp_kwargs.update(
             {
-                "field": ["impressions", "link_url_asset[website_url]", "actions[action_type:link_click]"],
+                "field": [
+                    "impressions",
+                    "link_url_asset[website_url]",
+                    "actions[action_type:link_click]",
+                ],
                 "breakdown": ["link_url_asset"],
-                "action_breakdown": ["action_type"]
+                "action_breakdown": ["action_type"],
             }
         )
         expected = ["impressions", "actions"]
         assert set(FacebookReader(**temp_kwargs)._api_fields) == set(expected)
-    
+
     def test_get_field_paths(self):
-        
+
         temp_kwargs = self.kwargs.copy()
         temp_kwargs.update(
             {
-                "field": ["impressions", "link_url_asset[website_url]", "actions[action_type:link_click]"],
+                "field": [
+                    "impressions",
+                    "link_url_asset[website_url]",
+                    "actions[action_type:link_click]",
+                ],
                 "breakdown": ["link_url_asset"],
-                "action_breakdown": ["action_type"]
+                "action_breakdown": ["action_type"],
             }
         )
-        expected = [["impressions"], ["link_url_asset", "website_url"], ["actions", "action_type:link_click"]]
+        expected = [
+            ["impressions"],
+            ["link_url_asset", "website_url"],
+            ["actions", "action_type:link_click"],
+        ]
         assert FacebookReader(**temp_kwargs)._field_paths == expected
 
     @mock.patch("nck.readers.facebook_reader.FacebookReader.query_ad_insights")
@@ -97,7 +143,9 @@ class FacebookReaderTest(TestCase):
     @mock.patch.object(FacebookAdsApi, "init", lambda *args: None)
     def test_read_with_ad_insights_query(self, mock_query_ad_insights):
         temp_kwargs = self.kwargs.copy()
-        temp_kwargs.update({"ad_insights": True, "field": ["date_start", "impressions"]})
+        temp_kwargs.update(
+            {"ad_insights": True, "field": ["date_start", "impressions"]}
+        )
 
         row1, row2 = AdsInsights(), AdsInsights()
         row1.set_data({"date_start": "2020-01-01", "impressions": "1"})
@@ -105,7 +153,10 @@ class FacebookReaderTest(TestCase):
         mock_query_ad_insights.return_value = [row1, row2]
 
         data = next(FacebookReader(**temp_kwargs).read())
-        expected = [{"date_start": "2020-01-01", "impressions": "1"}, {"date_start": "2020-01-01", "impressions": "2"}]
+        expected = [
+            {"date_start": "2020-01-01", "impressions": "1"},
+            {"date_start": "2020-01-01", "impressions": "2"},
+        ]
 
         for record, report in zip(data.readlines(), iter(expected)):
             assert record == report
@@ -124,8 +175,11 @@ class FacebookReaderTest(TestCase):
         mock_query_object_node.return_value = [row1, row2]
 
         data = next(FacebookReader(**temp_kwargs).read())
-        expected = [{"id": "123456789", "status": "ACTIVE"}, {"id": "987654321", "status": "PAUSED"}]
-        
+        expected = [
+            {"id": "123456789", "status": "ACTIVE"},
+            {"id": "987654321", "status": "PAUSED"},
+        ]
+
         for record, report in zip(data.readlines(), iter(expected)):
             assert record == report
 
@@ -133,44 +187,71 @@ class FacebookReaderTest(TestCase):
         [
             (
                 "simple_field",
-                {"field":["impressions"], "action_breakdown":[]},
+                {"field": ["impressions"], "action_breakdown": []},
                 {"impressions": "10314"},
-                {"impressions": "10314"}
+                {"impressions": "10314"},
             ),
             (
                 "nested_field",
-                {"field":["creative[id]"], "action_breakdown":[]},
+                {"field": ["creative[id]"], "action_breakdown": []},
                 {"creative": {"id": "123456789"}},
-                {"creative[id]": "123456789"}
+                {"creative[id]": "123456789"},
             ),
             (
                 "action_breakdown_field_without_filters",
-                {"field":["actions"], "action_breakdown":["action_type", "action_device"]},
-                {"actions": [
+                {
+                    "field": ["actions"],
+                    "action_breakdown": ["action_type", "action_device"],
+                },
+                {
+                    "actions": [
                         {"action_type": "link_click", "value": "0"},
-                        {"action_type": "post_engagement", "value": "1"}
+                        {"action_type": "post_engagement", "value": "1"},
                     ]
                 },
-                {"actions[action_type:link_click]": "0", "actions[action_type:post_engagement]": "1"}
+                {
+                    "actions[action_type:link_click]": "0",
+                    "actions[action_type:post_engagement]": "1",
+                },
             ),
             (
                 "action_breakdown_field_without_filters",
-                {"field":["actions[action_type:link_click][action_device:iphone]"],"action_breakdown":["action_type", "action_device"]},
-                {"actions": [
-                        {"action_type": "link_click", "action_device":"iphone", "value": "0"},
-                        {"action_type": "post_engagement", "action_device":"iphone", "value": "1"},
-                        {"action_type": "link_click", "action_device":"desktop", "value": "2"},
-                        {"action_type": "post_engagement", "action_device":"desktop", "value": "3"},
+                {
+                    "field": ["actions[action_type:link_click][action_device:iphone]"],
+                    "action_breakdown": ["action_type", "action_device"],
+                },
+                {
+                    "actions": [
+                        {
+                            "action_type": "link_click",
+                            "action_device": "iphone",
+                            "value": "0",
+                        },
+                        {
+                            "action_type": "post_engagement",
+                            "action_device": "iphone",
+                            "value": "1",
+                        },
+                        {
+                            "action_type": "link_click",
+                            "action_device": "desktop",
+                            "value": "2",
+                        },
+                        {
+                            "action_type": "post_engagement",
+                            "action_device": "desktop",
+                            "value": "3",
+                        },
                     ]
                 },
-                {"actions[action_type:link_click][action_device:iphone]": "0"}
+                {"actions[action_type:link_click][action_device:iphone]": "0"},
             ),
             (
                 "field_not_in_record",
-                {"field":["impressions", "clicks"], "action_breakdown":[]},
+                {"field": ["impressions", "clicks"], "action_breakdown": []},
                 {"impressions": "1"},
-                {"impressions": "1"}
-            )
+                {"impressions": "1"},
+            ),
         ]
     )
     def test_format_and_yield(self, name, parameters, record, expected):
