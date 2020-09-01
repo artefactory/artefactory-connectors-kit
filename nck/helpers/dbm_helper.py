7fb41cac9b54090b5536e2f04fea02d82d6be084
Vivien MORLET <vivienmorlet@FRART0119M.local>
2020-09-01 16:22:01 +0200

    upgrade DBM v1.1 - update get_query and remove sdf requests

--- nck/helpers/dbm_helper.py
+++ nck/helpers/dbm_helper.py
@@ -21,23 +21,5 @@ POSSIBLE_REQUEST_TYPES = [
     "existing_query_report",
     "custom_query_report",
     "lineitems_objects",
-    "sdf_objects",
     "list_reports",
 ]
-
-POSSIBLE_SDF_FILE_TYPES = [
-    "INVENTORY_SOURCE",
-    "AD",
-    "AD_GROUP",
-    "CAMPAIGN",
-    "INSERTION_ORDER",
-    "LINE_ITEM",
-]
-
-FILE_TYPES_DICT = {
-    "AD": "ads",
-    "AD_GROUP": "adGroups",
-    "CAMPAIGN": "campaigns",
-    "LINE_ITEM": "lineItems",
-    "INSERTION_ORDER": "insertionOrders",
-}
