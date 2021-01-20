2366949c5ecb26ec9b8602d2eb7962b9c399db75
tom-grivaud <tom.grivaud@artefact.com>
2021-01-20 12:28:31 +0100

    fix: rmv useless tests

--- tests/readers/test_mytarget_reader.py
+++ tests/readers/test_mytarget_reader.py
@@ -21,48 +21,15 @@ class TestMyTargetReader(TestCase):
     @freeze_time("2021-01-18")
     @mock.patch.object(MyTargetReader, "__init__", mock_mytarget_reader)
     def test_check_date_not_in_future(self):
-        kwargs = {}
-        reader = MyTargetReader(**kwargs)
-        reader.kwargs = self.kwargs
+        reader = MyTargetReader(**self.kwargs)
         with self.assertRaises(ValueError):
             reader.__check_date_not_in_future(datetime.datetime(2024, 1, 1, 0, 0))
         assert self.assertTrue(reader.__check_date_not_in_future(datetime.datetime(2021, 1, 18, 0, 0)))
         assert self.assertTrue(reader.__check_date_not_in_future(datetime.datetime(2020, 1, 17, 0, 0)))
 
     @mock.patch.object(MyTargetReader, "__init__", mock_mytarget_reader)
-    def test_check_validity_date(self):
-        kwargs = {}
-        reader = MyTargetReader(**kwargs)
-        reader.kwargs = self.kwargs
-        with self.assertRaises(ValueError):
-            reader.__check_date_not_in_future(datetime.datetime(2024, 1, 1, 0, 0))
-        with self.assertRaises(ValueError):
-            reader.__check_date_not_in_future(datetime.datetime(2020, 13, 1, 0, 0))
-        with self.assertRaises(ValueError):
-            reader.__check_date_not_in_future(datetime.datetime(2020, 12, 100, 0, 0))
-        assert self.assertTrue(reader.__check_date_not_in_future(datetime.datetime(2020, 3, 13, 0, 0)))
-
-    @mock.patch.object(MyTargetReader, "__init__", mock_mytarget_reader)
-    def test_check_both_start_end_valid(self):
-        kwargs = {}
-        reader = MyTargetReader(**kwargs)
-        reader.kwargs = self.kwargs
-        with self.assertRaises(ValueError):
-            reader.__check_both_start_end_valid(None, datetime.datetime(2020, 1, 1, 0, 0))
-        with self.assertRaises(ValueError):
-            reader.__check_both_start_end_valid(datetime.datetime(2020, 1, 1, 0, 0), None)
-        with self.assertRaises(ValueError):
-            reader.__check_both_start_end_valid(None, None)
-        assert self.assertTrue(reader.__check_both_start_end_valid(
-            datetime.datetime(2020, 3, 13, 0, 0),
-            datetime.datetime(2020, 3, 13, 0, 0)
-        ))
-
-    @mock.patch.object(MyTargetReader, "__init__", mock_mytarget_reader)
     def test_check_end_posterior_to_start(self):
-        kwargs = {}
-        reader = MyTargetReader(**kwargs)
-        reader.kwargs = self.kwargs
+        reader = MyTargetReader(**self.kwargs)
         with self.assertRaises(ValueError):
             reader.__check_end_posterior_to_start(datetime.datetime(2020, 1, 1, 0, 0), datetime.datetime(2019, 12, 31, 0, 0))
         with self.assertRaises(ValueError):
