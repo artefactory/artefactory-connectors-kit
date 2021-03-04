02c0d133eab50f6a585eb4bbabc25a609683d167
Vivien MORLET <vivien.morlet@artefact.com>
2021-03-04 12:56:27 +0100

    add end_date >= start_date check

--- nck/utils/date_handler.py
+++ nck/utils/date_handler.py
@@ -40,12 +40,16 @@ DEFAULT_DATE_RANGE_FUNCTIONS = {
 
 
 def check_date_range_definition_conformity(start_date: date, end_date: date, date_range: str):
-    if not any([start_date, end_date, date_range]):
-        raise NoDateDefinitionException("You must at least define a couple (start-date, end-date) or a date-range")
-    elif bool(start_date) != bool(end_date):
+    if bool(start_date) != bool(end_date):
         raise MissingDateDefinitionException("Both start_date and end_date must be defined")
-    elif all([start_date, end_date, date_range]):
-        raise InconsistentDateDefinitionException("You must define either (start_date, end_date) or date_range but not both")
+    if date_range:
+        if all([start_date, end_date]):
+            raise InconsistentDateDefinitionException("You must define either (start_date, end_date) or date_range")
+    else:
+        if not any([start_date, end_date]):
+            raise NoDateDefinitionException("You must at least define a couple (start-date, end-date) or a date-range")
+        elif end_date < start_date:
+            raise InconsistentDateDefinitionException("Report end date should be equal or ulterior to report start date.")
 
 
 def get_date_start_and_date_stop_from_date_range(date_range: str) -> Tuple[date, date]:
