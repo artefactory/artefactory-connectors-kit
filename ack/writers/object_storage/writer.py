7bd2946535c9cacb4a09a50662b0061e9da2f965
Louis Rousselot de Saint Ceran <louis.rousselot-de-saint-ceran@artefact.com>
2021-03-31 20:06:37 +0200

    hotfix removing retry from gcs writer

--- ack/writers/object_storage/writer.py
+++ ack/writers/object_storage/writer.py
@@ -19,7 +19,8 @@ import os
 
 from ack.config import logger
 from ack.writers import Writer
-from ack.utils.retry import retry
+
+# from ack.utils.retry import retry
 
 
 class ObjectStorageWriter(Writer):
@@ -36,7 +37,6 @@ class ObjectStorageWriter(Writer):
         final_name = os.path.join(self._prefix, self._file_name)
         self._write_aux(stream, final_name)
 
-    @retry
     def _write_aux(self, stream, final_name):
         self._create_blob(final_name, stream)
         logger.info(f"Wrote {final_name} file to {self._bucket_name} on  {self._platform} ...")
