8506db2ebc5f4f7775aba5cdac1380e9d8194813
Brian Davis <briandavis9066@gmail.com>
2022-01-19 09:25:50 +0000

    adjusted build_url based on suggestion

--- ack/readers/awin_advertiser/reader.py
+++ ack/readers/awin_advertiser/reader.py
@@ -40,20 +40,32 @@ class AwinAdvertiserReader(Reader):
     @retry
     def request(self):
         if self.report_type == REPORT_TYPES[0]:
-            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}"\
-                f"/reports/{self.report_type}?startDate={self.start_date}&endDate={self.end_date}"\
-                f"&timezone={self.timezone}&accessToken={self.auth_token}"
+            payload = {
+                'startDate': self.start_date, 
+                'endDate': self.end_date, 
+                'timezone': self.timezone, 
+                'accessToken': self.auth_token
+                }
+            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}/reports/{self.report_type}?"
         elif self.report_type == REPORT_TYPES[1]:
-            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}"\
-                f"/reports/{self.report_type}?startDate={self.start_date}&endDate={self.end_date}"\
-                f"&region={self.region}&timezone={self.timezone}&accessToken={self.auth_token}"
+            payload = {
+                'startDate': self.start_date, 
+                'endDate': self.end_date, 
+                'region': self.region,
+                'timezone': self.timezone, 
+                'accessToken': self.auth_token
+                }
+            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}/reports/{self.report_type}?"
         elif self.report_type == REPORT_TYPES[2]:
-            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}"\
-                f"/reports/{self.report_type}?startDate={self.start_date}&endDate={self.end_date}"\
-                f"&accessToken={self.auth_token}"
+            payload = {
+                'startDate': self.start_date, 
+                'endDate': self.end_date, 
+                'accessToken': self.auth_token
+                }
+            build_url = f"{AWIN_API_ENDPOINT}{self.advertiser_id}/reports/{self.report_type}?"
 
         response = requests.get(
-            build_url
+            build_url, params=payload
         )
         json_response = response.json()
         logger.debug(f"Response: {json_response}")
